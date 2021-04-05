5d0bab00094215dd41ad0ff2951dda3bee01b71a
abdalmajeed-zeyad <abd.d@live.com>
2021-04-05 15:51:47 +0300

    a

---
+++ javascript/app.js
@@ -0,0 +1,9 @@
+var a = prompt("Please enter your name")
+var b = prompt("Please enter your living area")
+var c = prompt("Please enter your age")
+var d = prompt("Please enter your job description")
+
+
+alert('hello '+a+',we love '+b+',your age is  '+c+',it is a good job mr '+d)
+console.log(a,b,c,d)
+
