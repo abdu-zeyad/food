e61b074cf7d0542226fb9b2acbcf1e1035f6d742
abdu-zeyad <abd.d@live.com>
2021-04-06 10:01:32 +0300

    a

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,4 @@
+
 var a = prompt("Please enter your name")
 var b = prompt("Please enter your living area")
 var c = prompt("Please enter your age")
---
+++ javascript/java.js
@@ -0,0 +1,16 @@
+'use strict';
+// truthy and falsy 
+
+// true is truthy value 
+// string is truthy and empty string is falsy
+
+
+// falsey:NaN NOT A NUMBER, empty variable 
+let value = 0 ;
+if (value) {
+    console.log('this is truthy value')
+} else {
+    
+    console.log('this is falsey value')
+
+}
