df66b7543b4a0b382cae70c42f9dd41c8b8f2de5
Abelmajeed Alabedalraheem <abd.d@live.com>
2021-04-08 12:42:43 +0300

    a

--- javascript/app.js
+++ javascript/app.js
@@ -5,6 +5,3 @@ var c = prompt("Please enter your age")
 var d = prompt("Please enter your job description")
 
 
-alert('hello '+a+',we love '+b+',your age is  '+c+',it is a good job mr '+d)
-console.log(a,b,c,d)
-
