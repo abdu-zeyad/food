e144331a798c2a324384a078435719f4f2c4acef
Abelmajeed Alabedalraheem <81154281+abdu-zeyad@users.noreply.github.com>
2021-04-09 16:05:18 +0300

    4

--- javascript/app.js
+++
@@ -1,7 +0,0 @@
-
-var a = prompt("Please enter your name")
-var b = prompt("Please enter your living area")
-var c = prompt("Please enter your age")
-var d = prompt("Please enter your job description")
-
-
